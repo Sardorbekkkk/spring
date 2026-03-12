89a7ee637347dce70b270d7b2440266b2099478e
Sardorbekkkk <sardorthekadirov@gmail.com>
2026-03-12 21:33:34 +0300

    add resController

---
+++ src/main/java/com/example/demo/book/controller/BookApiController.java
@@ -0,0 +1,15 @@
+package com.example.demo.book.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BookApiController {
+
+
+    @GetMapping
+    public String  ok(){
+        return "ok";
+    }
+}
